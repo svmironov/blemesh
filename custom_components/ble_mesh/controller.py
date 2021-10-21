7c85d5e233ffa2b571188daf95f0d323e6102d2c
Sergey Mironov <se.mironov@x5.ru>
2021-10-21 13:58:10 +0300

    Bug fixes

--- custom_components/ble_mesh/controller.py
+++ custom_components/ble_mesh/controller.py
@@ -1,4 +1,4 @@
-import threading
+import time
 from bluepy import btle
 from bluepy.btle import BTLEDisconnectError
 from .const import CMD_FUNCTION, CMD_ON, CMD_OFF, CMD_GPIO_CONTROL, CMD_OUT_1, HANDLE_ID
@@ -9,7 +9,6 @@ class Device:
         self._name = name
         self._mac = mac
         self._mesh_id = mesh_id
-        self._lock = False;
 
     def turn_on(self):
         self.__execute(CMD_ON)
@@ -18,21 +17,6 @@ class Device:
         self.__execute(CMD_OFF)
 
     def __execute(self, cmd):
-        if not self._lock:
-            self._lock = True
-            attempts = 3
-            for i in range(attempts):
-                try:
-                    self.__send_сmd(cmd)
-                except BTLEDisconnectError as e:
-                    if i < attempts - 1:
-                        continue
-                    else:
-                        raise
-                break
-            self._lock = False
-
-    def __send_сmd(self, cmd):
         frame = bytearray()
         frame.append(CMD_FUNCTION)
         frame.append(self._mesh_id)
@@ -40,6 +24,13 @@ class Device:
         frame.append(CMD_OUT_1)
         frame.append(cmd)
 
-        peripheral = btle.Peripheral(self._mac, btle.ADDR_TYPE_PUBLIC)
-        peripheral.writeCharacteristic(HANDLE_ID, frame, False)
+        successful = False
+        while successful is False:
+            try:
+                peripheral = btle.Peripheral(self._mac, btle.ADDR_TYPE_PUBLIC)
+                peripheral.writeCharacteristic(HANDLE_ID, frame, True)
+                peripheral.waitForNotifications(1.0)
+                successful = True
+            except BTLEDisconnectError as e:
+                time.sleep(0.5)
         
